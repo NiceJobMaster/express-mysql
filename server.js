c66295ef7afd51deccfce801377e4570e2a3c3b7
Kamil Marszałek <kamil.marszalek@netins.it>
2020-01-31 16:03:44 +0100

    add headers

--- server.js
+++ server.js
@@ -8,6 +8,20 @@ app.set("port", process.env.port || PORT);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 app.get("/", (req, res) => {
   res.json({ message: "welcome message" });
 });
